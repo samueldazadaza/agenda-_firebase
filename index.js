5ce246eeee0d8d9940cf011658e1791e1383f88a
Samuel Daza <samueldazadaza@gmail.com>
2021-06-16 22:58:06 -0500

    creando llamado a componentes necesarios

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from 'express'
+
+const app = express()
+
+app.listen('8000', (req, res) => {
+    console.log('aplicacion iniciada en localhost://8000')
+})
+
+app.set('views', ' ./vistas' )
+app.set('view engine', 'ejs')
+
+app.use(express.static(' ./estilos'))
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
