d93fb0ca72231e8b52928545843865013719d0ce
Samuel Daza <samueldazadaza@gmail.com>
2021-06-22 23:25:00 -0500

    funciona hasta 06:40

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.listen('8000', (req, res) => {
     console.log('aplicacion iniciada en localhost://8000')
 })
 
-app.set('views', ' ./vistas' )
+app.set('views', './vistas' )
 app.set('view engine', 'ejs')
 
 app.use(express.static(' ./estilos'))
